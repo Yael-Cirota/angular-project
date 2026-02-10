220632550ddd03b525e5fe8c6efdcd38fa1df539
Yael-Cirota <yael87226@gmail.com>
2026-02-10 21:34:51 +0200

    update filters

--- src/app/components/show-products/filters/filters.ts
+++ src/app/components/show-products/filters/filters.ts
@@ -1,28 +1,49 @@
-import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { CheckboxModule } from 'primeng/checkbox';
-import { AccordionModule } from 'primeng/accordion';
-import { ButtonModule } from 'primeng/button';
-import { CategoriesService } from '../../../services/categories-service';
+// import { Component, inject } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { FormsModule } from '@angular/forms';
+// import { CheckboxModule } from 'primeng/checkbox';
+// import { AccordionModule } from 'primeng/accordion';
+// import { ButtonModule } from 'primeng/button';
+// import { CategoriesService } from '../../../services/categories-service';
 
+
+// @Component({
+//   selector: 'app-filters',
+//   imports: [CommonModule, FormsModule, CheckboxModule, AccordionModule, ButtonModule],
+//   templateUrl: './filters.html',
+//   styleUrl: './filters.scss',
+// })
+// export class Filters {
+
+//   categoriesService = inject(CategoriesService);
+
+//   widths = ['XXN', 'XN', 'N', 'M', 'W', 'XW', 'XXW'];
+//   Categories = this.categoriesService.categories;
+//   amazonBrands = ['המותגים שלנו', 'מותגי פרימיום', 'מותגים מובילים'];
+
+//   // משתנים לשמירת הבחירות
+//   selectedWidths: string[] = [];
+//   selectedBrands: string[] = [];
+//   selectedAmazon: string[] = [];
+// }
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-filters',
-  imports: [CommonModule, FormsModule, CheckboxModule, AccordionModule, ButtonModule],
+  selector: 'app-filter-sidebar',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './filters.html',
-  styleUrl: './filters.scss',
+  styleUrl: './filters.scss'
 })
 export class Filters {
+  // כאן תוכל להוסיף פונקציות לסינון
+  selectedSize: string = '01';
 
-  categoriesService = inject(CategoriesService);
-
-  widths = ['XXN', 'XN', 'N', 'M', 'W', 'XW', 'XXW'];
-  Categories = this.categoriesService.categories;
-  amazonBrands = ['המותגים שלנו', 'מותגי פרימיום', 'מותגים מובילים'];
-
-  // משתנים לשמירת הבחירות
-  selectedWidths: string[] = [];
-  selectedBrands: string[] = [];
-  selectedAmazon: string[] = [];
+  onSizeSelect(size: string) {
+    this.selectedSize = size;
+    // שלח אירוע לקומפוננטה האבא כדי לעדכן את רשימת המוצרים
+  }
 }
+
